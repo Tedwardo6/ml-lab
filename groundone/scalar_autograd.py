eb370853752eb3a99911b551348ba363d2f6382f
Tedwardo6 <theodoreveteto@gmail.com>
2025-12-08 13:36:56 -0800

    2

--- groundone/scalar_autograd.py
+++ groundone/scalar_autograd.py
@@ -6,11 +6,53 @@ class Tensor:
         self.op = op
 
     def __repr__(self):
-        return f"Value(value={self.value}, grad={self.grad}, op={self.op})"
+        return f"Tensor(value={self.value}, grad={self.grad}, op={self.op})"
     
+    def add(self,other):
+        grad_place = 0
+        temp = self.value + other.value
+        par = [self,other]
+        return Tensor(temp,grad_place,par,"add")
+        
+    def mul(self,other):
+        grad_place = 0
+        temp = self.value * other.value
+        par = [self,other]
+        return Tensor(temp,grad_place,par,"mul")
 
-    #def backward(self):
+    def backward(self,upstream): #pass 1 into final.backward(1)
+        self.grad += upstream
+        if self.op == "mul":
+            x = self.parents[0]
+            y = self.parents[1]
+            grad_to_x = 0
+            grad_to_y = 0
+            grad_to_x += self.grad * y.value    
+            grad_to_y += self.grad * x.value   #error propogating duplicated objects, dont use self.grad for contributions just for storage
+            x.backward(grad_to_x)
+            y.backward(grad_to_y)
+        elif self.op == "add":
+            x = self.parents[0]
+            y = self.parents[1]
+            grad_to_x = 0
+            grad_to_y = 0     
+            grad_to_x += self.grad
+            grad_to_y += self.grad
+            x.backward(grad_to_x)
+            y.backward(grad_to_y)
+        elif self.op == None:
+            return
 
 x = Tensor(2.0)
+y = Tensor(3.0)
+k = Tensor(5.0)
 
-print(x)
+z = x.add(y)
+g = z.mul(z)
+
+
+g.backward(1)
+
+print(g)
+print(g.parents)
+print(z.parents)
