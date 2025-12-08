3a3cc7eb97d66689d9d0cebd9236b2927786a6d4
Tedwardo6 <theodoreveteto@gmail.com>
2025-12-07 17:13:06 -0800

    1

--- groundzero/binarysearch.py
+++ groundzero/binarysearch.py
@@ -3,6 +3,7 @@ import random
 
 ARRAY_SIZE = random.randint(5,50)
 unsorted = []
+sorted = []
 
 for i in range(ARRAY_SIZE):
     unsorted.append(random.randint(1,150)) 
@@ -22,5 +23,32 @@ def sort():
         swap_count = 0
         num_passes += 1
 
+def search():
+    
+    x = int(input("Input the number you want to search for: "))
+    flag = False
+    lower = 0
+    upper = len(unsorted)-1
+    midpoint = (upper + lower)//2
+
+    while lower <= upper:
+
+        midpoint = (upper + lower)//2
+
+        if unsorted[midpoint] == x:
+            print("Your number is located at i =", midpoint)
+            break
+        elif x > unsorted[midpoint]:
+            upper = midpoint - 1
+        elif x < unsorted[midpoint]:
+            lower = midpoint + 1
+    
+    if x != unsorted[midpoint]:
+        print("Number not found.")
+    
+  
+sort()
+print(unsorted)
+search()
 
 
